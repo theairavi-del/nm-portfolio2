91e629fec712c646cd115f7da741673feb869e1e
Ravi <raviclaw@Ravis-MacBook-Pro.local>
2026-02-21 00:11:39 -0500

    Randomize images on every page refresh

--- app.js
+++ app.js
@@ -885,11 +885,20 @@ function renderCards(images) {
   emptyState.hidden = true;
 }
 
+function shuffleArray(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
 function syncCards(images) {
-  allImages = images;
-  imagesState = images;
+  allImages = shuffleArray(images);
+  imagesState = allImages;
   imageIndexByName.clear();
-  images.forEach((image, index) => imageIndexByName.set(image.name, index));
+  allImages.forEach((image, index) => imageIndexByName.set(image.name, index));
   
   // Calculate initial display count
   displayedCount = Math.min(INITIAL_LOAD_COUNT, images.length);
